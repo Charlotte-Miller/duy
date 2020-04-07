b6d84c29b27f609bd8652f33cf727e6c95205ab4
hungdevjs <hungdev.js@gmail.com>
2020-04-07 21:39:52 +0700

    init

---
+++ index.js
@@ -0,0 +1,137 @@
+// let a =Number(prompt('Nhap vao 1 so bat ky'));
+
+//  for (let c = 2 ;c<a;c++){
+//      if(c===2){
+//          console.log(c);
+//      }
+//      else if (c===3){
+//         console.log(c);
+//      } else {
+//      let count = 0;
+//      for (let i =2 ; i<=Math.sqrt(c);i++){
+//          if (c%i===0){
+//              count ++;
+             
+//          }
+        
+//      }if (count===0){
+//         console.log(c);
+//  } 
+// }
+// }
+
+
+
+
+// let a = Number(prompt('Nhap vao 1 so bat ky'));
+// let tong = 0;
+// for (let i=0;i<=a;i++){
+    
+  
+//     if (i%2==1){
+//         tong+=i
+//     }
+    
+// }console.log('Tong cac so le la '+tong);
+
+// let tongLe = 0;
+// for (let j=0;j<=a;j++){
+    
+  
+//     if (j%2==0){
+//         tongLe+=j
+//     }
+    
+// }console.log(tongLe);
+
+
+
+
+// let a = Number(prompt('Nhap vao 1 so bat ky'));
+// let count =0;
+// for (let i =2;i<=Math.sqrt(a);i++){
+//     if (a%i==0){
+//         count++
+//     }
+// }if (count===0){
+//     console.log(true);
+// }else {
+//     console.log(false);
+// }
+
+
+// let a = Number(prompt('Nhap vao 1 so bat ky'));
+// let i=1,j=0,temp;
+// for (let k=a;k=1;k--){
+//     temp=i;
+//     i=i+j;
+//     j=temp;
+   
+    
+// }console.log(j);
+// function fibonaci(a) {
+//     if (a<=2){
+//         return 1;
+//     }
+//     else return fibonaci(a-2)+ fibonaci(a-1);
+// }
+
+// console.log(fibonaci(7));
+
+
+
+
+
+
+    // loop=true;
+    // let tong=0;
+    // while (loop){
+    //     let a = Number(prompt('Nhap vao so bat ky'));
+    //     if (a>=0){
+    //         tong+=a;
+    //     }
+    //     else {
+    //         console.log(tong);
+    //         break ;
+    //     }
+    // }
+    
+
+
+    // let a = prompt('Nhap chuoi bat ky');
+    
+    // let b = a.split("").reverse().join('');
+    // if (b===a){
+    //     console.log(true) ;
+
+    // }else {
+    //     console.log(false);
+    // }
+    
+    // let a = prompt('Nhap chuoi bat ky');
+    // let b = length.a ;
+    // let count = 0;
+    // for (let i=1;i<b/2;i++){
+    //     if (a[i]!==a[b+1-i]){
+    //         count ++;
+    //     }
+    // } if (count ==0){
+    //     console.log('chuoi doi xung');
+    //  } else {
+    //         console.log('chuoi k doi xung')
+    //     }
+    
+    // let m = Number(prompt('Nhap vao chieu dai hcn'));
+    // let n = Number(prompt('Nhap vao chieu rong hcn'));
+    // let count = '';
+    // for (i=1;i<=m;i++){
+        
+    //     for (j=1;j<=n;j++){
+    //         count += '*';
+            
+    //     }count += "\n";
+        
+    // }console.log(count);
+    
+
+    
